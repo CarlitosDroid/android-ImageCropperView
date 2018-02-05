a65e36f8194618c77e6df68eaf9d73c4402a5c87
Carlos Leonardo Camilo <carlosleonardocamilovargashuaman@gmail.com>
2018-02-04 23:49:52 -0500

    wip(imagecropperview): Add new functionaly zoom in and zoom out

--- materialimagecropper/src/main/java/com/spidev/materialimagecropper/ImageCropperView.kt
+++ materialimagecropper/src/main/java/com/spidev/materialimagecropper/ImageCropperView.kt
@@ -8,6 +8,7 @@ import android.net.Uri
 import android.provider.MediaStore
 import android.util.AttributeSet
 import android.util.Log
+import android.view.GestureDetector
 import android.view.MotionEvent
 import android.view.ScaleGestureDetector
 import android.view.View
@@ -20,6 +21,14 @@ import android.widget.Toast
 
 class ImageCropperView : View {
 
+    /**
+     * For handling movement on drawable image
+     */
+    private lateinit var gestureDetector: GestureDetector
+
+    /**
+     * For handling the zoom in and zoom out on the drawable image
+     */
     private lateinit var scaleGestureDetector: ScaleGestureDetector
 
     private var gridDrawable = GridDrawable()
@@ -58,23 +67,11 @@ class ImageCropperView : View {
     private val rectF = RectF()
 
     /**
-     * These variables save the initial position and the last position of the drawable image
+     * The displacement of the image
+     * we only need the left and top because the others can be calculated from these
      */
-    private var rawX = 0f
-    private var rawY = 0f
-
-    /**
-     * dxAtEachNewPoint is our displacement X-axis at each new point
-     * dyAtEachNewPoint is our displacement Y-axis at each new point
-     * these values can be negative or positive depends on direction
-     * for instance:
-     * (20, 10) initial position(ACTION_DOWN) -> (25, 5) final position(ACTION_MOVE)
-     * dxAtEachNewPoint is 25 - 20 = 5, dyAtEachNewPoint is 5 - 10 = -5
-     * (25, 5) (last position) -> (31, 0) (final position)
-     * dxAtEachNewPoint = 6, dyAtEachNewPoint = -5
-     */
-    private var dxAtEachNewPoint = 0f
-    private var dyAtEachNewPoint = 0f
+    private var mDisplayDrawableLeft = 0f
+    private var mDisplayDrawableTop = 0f
 
     constructor(context: Context) : super(context)
 
@@ -91,24 +88,8 @@ class ImageCropperView : View {
         mAnimator!!.duration = 400
         mAnimator!!.setFloatValues(0f, 1f)
         mAnimator!!.interpolator = DecelerateInterpolator(0.25f)
-        mAnimator!!.addUpdateListener { animation ->
-            val animatedValue = animation.animatedValue as Float
-
-            val overScrollX = measureOverScrollX()
-            val overScrollY = measureOverScrollY()
-
-            // for example image that user move the image (100, 0) and then ACTION UP
-            // (overScrollX * animatedValue) increase its value from 0 to 50 and then 50 to 0
-            // between the duration time setted only for showing an velocity effect
-            rectF.left -= (overScrollX * animatedValue)
-            rectF.right = rectF.left + (drawableImageWidth * drawableImageScale)
-
-            rectF.top -= (overScrollY * animatedValue)
-            rectF.bottom = rectF.top + (drawableImageHeight * drawableImageScale)
-
-            invalidate()
-        }
-
+        mAnimator!!.addUpdateListener(onSettleAnimatorUpdateListener)
+        gestureDetector = GestureDetector(context, onGestureListener)
         scaleGestureDetector = ScaleGestureDetector(context, onScaleGestureListener)
     }
 
@@ -251,6 +232,13 @@ class ImageCropperView : View {
          * VER LA DIFERENCIA ENTRE RECTF Y RECT - ---> porque setBounds tambien recive un rectangulo
          */
 
+
+        rectF.left = mDisplayDrawableLeft
+        rectF.top = mDisplayDrawableTop
+        rectF.right = rectF.left + (drawableImageScale * drawableImageWidth)
+        rectF.bottom = rectF.top + (drawableImageScale * drawableImageHeight)
+
+
         bitmapDrawable?.setBounds(rectF.left.toInt(), rectF.top.toInt(), rectF.right.toInt(), rectF.bottom.toInt())
         bitmapDrawable?.draw(canvas)
         gridDrawable.draw(canvas)
@@ -341,36 +329,12 @@ class ImageCropperView : View {
      */
     override fun onTouchEvent(event: MotionEvent): Boolean {
 
-        //Detector handling event like zoom in or zoom out on the drawable image
+        gestureDetector.onTouchEvent(event)
         scaleGestureDetector.onTouchEvent(event)
 
         when (event.actionMasked) {
-            MotionEvent.ACTION_DOWN -> {
-                rawX = event.rawX
-                rawY = event.rawY
-            }
-
-            MotionEvent.ACTION_MOVE -> {
-                dxAtEachNewPoint = event.rawX - rawX
-                dyAtEachNewPoint = event.rawY - rawY
-
-                dxAtEachNewPoint = applyOverScrollFix(dxAtEachNewPoint, measureOverScrollX())
-                dyAtEachNewPoint = applyOverScrollFix(dyAtEachNewPoint, measureOverScrollY())
-
-                rectF.left += dxAtEachNewPoint
-                rectF.right += dxAtEachNewPoint
-
-                rectF.top += dyAtEachNewPoint
-                rectF.bottom += dyAtEachNewPoint
-
-                invalidate()
-
-                //save the last position for getting the new displacement
-                rawX = event.rawX
-                rawY = event.rawY
-            }
-
             MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL, MotionEvent.ACTION_OUTSIDE -> {
+                Log.e("ACTION UP", "ACTION UP")
                 mAnimator!!.start()
             }
         }
@@ -480,7 +444,46 @@ class ImageCropperView : View {
     }
 
     /**
+     * Listener for handling movement of the user on the drawable image
+     */
+    private var onGestureListener = object : GestureDetector.OnGestureListener {
+
+        override fun onShowPress(e: MotionEvent?) {
+        }
+
+        override fun onSingleTapUp(e: MotionEvent?): Boolean {
+            return false
+        }
+
+        override fun onDown(e: MotionEvent?): Boolean {
+            return true
+        }
+
+        override fun onFling(e1: MotionEvent?, e2: MotionEvent?, velocityX: Float, velocityY: Float): Boolean {
+            return false
+        }
+
+        override fun onScroll(e1: MotionEvent?, e2: MotionEvent?, distanceX: Float, distanceY: Float): Boolean {
+            var mDistanceX = -distanceX
+            var mDistanceY = -distanceY
+
+            mDistanceX = applyOverScrollFix(mDistanceX, measureOverScrollX())
+            mDistanceY = applyOverScrollFix(mDistanceY, measureOverScrollY())
+
+            mDisplayDrawableLeft += mDistanceX
+            mDisplayDrawableTop += mDistanceY
+
+            invalidate()
+            return true
+        }
+
+        override fun onLongPress(e: MotionEvent?) {
+        }
+    }
+
+    /**
      * This listener reacts when the user touch the drawable image with 2 or more fingers
+     * with this listener we can handle the zoom in and zoom out of the image
      */
     private var onScaleGestureListener = object : ScaleGestureDetector.OnScaleGestureListener {
         override fun onScaleBegin(detector: ScaleGestureDetector?): Boolean {
@@ -491,54 +494,71 @@ class ImageCropperView : View {
         }
 
         override fun onScale(detector: ScaleGestureDetector): Boolean {
+            //TODO ANALYSING THIS METHOD
+            //imageScale 1.5
+            //overScale 1.8
 
             //tiendre a crecer
             // overScale = drawableImageScale / 0.8
 
-            var overScale = measureOverScale()
+            val overScale = measureOverScale()
 
-            //Log.e("FACTOR" , "FACTOR " + detector.scaleFactor)
+            //cuando expando es 1.000
+            //cuando collapso es 0.90000
+            //Log.e("FACTOR" , "FACTOR " + mScaleFactor)
             //Log.e("FOCUSX" , "FOCUSX " + detector.focusX)
             //Log.e("FOCUSY" , "FOCUSY " + detector.focusY)
 
-            Log.e("OVERSCALE" , "OVERSCALE " + overScale)
+            val scale = applyOverScaleFix(detector.scaleFactor, overScale)
 
-            var scale = applyOverScaleFix(detector.scaleFactor, overScale)
+            drawableImageScale *= scale
 
-            drawableImageScale = drawableImageScale * scale
+            rectF.left = mDisplayDrawableLeft
+            rectF.top = mDisplayDrawableTop
 
-            //setScaleKeepingFocus(detector.focusX, detector.focusY)
-
-
-
+            //Log.e("LEFFFT ","LEFFTT " + rectF.left)
+            Log.e("TOPP ", "TOPP " + rectF.top)
+            //Log.e("RIGHT ","RIGHT " + bounds.right);
+            //Log.e("BOTTOM ","BOTTOM " + mDisplayDrawableLeft);
 
             invalidate()
             return true
         }
-
     }
 
     private val MAXIMUM_OVER_SCALE = 0.7f
 
+    //TODO ANALYZING THIS CODE
+    /**
+     * Returning the scale
+     */
     private fun applyOverScaleFix(scaleFactor: Float, overScale: Float): Float {
         var mOverScale = overScale
         var mScaleFactor = scaleFactor
 
+        //si no hay over scale
         if (mOverScale == 1f) {
             return mScaleFactor
         }
 
+        //
         if (mOverScale > 1) {
             mOverScale = 1f / mOverScale
         }
 
-        val wentOverScaleRatio = (overScale - MAXIMUM_OVER_SCALE) / (1 - MAXIMUM_OVER_SCALE)
+        val wentOverScaleRatio = (mOverScale - MAXIMUM_OVER_SCALE) / (1 - MAXIMUM_OVER_SCALE)
 
         mScaleFactor *= wentOverScaleRatio + (1 - wentOverScaleRatio) / scaleFactor
 
         return scaleFactor
     }
 
+    /**
+     * Nuestra imagen puede sobreescalarse un valor entre 0.83f
+     * for instance:
+     * esto hace que si nuestra scala inicial es 1.5 aumente hasta 1.8
+     * 1.5 / 0.83 = 1.8
+     */
     private fun measureOverScale(): Float {
         if (drawableImageScale < 0.83f) {
             return drawableImageScale / 0.83f
@@ -547,23 +567,28 @@ class ImageCropperView : View {
         if (drawableImageScale > 0.83f) {
             return drawableImageScale / 0.83f
         }
-
         return 1f
     }
 
-    private fun setScaleKeepingFocus(focusX: Float, focusY: Float) {
 
-        val focusRatioX = (focusX - rectF.left) / rectF.width()
-        val focusRatioY = (focusY - rectF.top) / rectF.height()
+    /**
+     * Listener for settling drawable image after user ACTION UP
+     */
+    private var onSettleAnimatorUpdateListener = object: ValueAnimator.AnimatorUpdateListener{
+        override fun onAnimationUpdate(animation: ValueAnimator) {
+            val animatedValue = animation.animatedValue as Float
 
-        val scaledFocusX = rectF.left + focusRatioX * rectF.width()
-        val scaledFocusY = rectF.top + focusRatioY * rectF.height()
+            val overScrollX = measureOverScrollX()
+            val overScrollY = measureOverScrollY()
 
-        //LogUtil.e("SCALE", "SCALE ${focusX - scaledFocusX}")
+            // for example image that user move the image (100, 0) and then ACTION UP
+            // (overScrollX * animatedValue) increase its value from 0 to 50 and then 50 to 0
+            // between the duration time setted only for showing an velocity effect
+            mDisplayDrawableLeft -= overScrollX * animatedValue
+            mDisplayDrawableTop -= overScrollY * animatedValue
 
-        rectF.left += focusX - scaledFocusX
-        rectF.top += focusY - scaledFocusY
-
+            invalidate()
+        }
     }
 }
 
